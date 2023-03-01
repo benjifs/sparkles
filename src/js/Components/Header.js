0d14da90d393741b54bd14b2502a80ec8c2ff000
Benji Encalada Mora <benjifs@users.noreply.github.com>
2023-03-01 10:26:46 -0600

    fix: svg in Header

--- src/js/Components/Header.js
+++ src/js/Components/Header.js
@@ -6,7 +6,7 @@ const Header = {
 			m('.h-x-app.h-app', [
 				m('a.u-url', { href: '/' }, [
 					m('img.u-logo.p-name', {
-						src: '/assets/icons/favicon-144x144.png',
+						src: '/assets/logo.svg',
 						alt: 'sparkles'
 					})
 				])
